809788e917ea561f0d105d98f8d96f4a6cdcfb93
gonzaloAq <62177431+gonzaloAq@users.noreply.github.com>
2020-07-21 23:45:30 -0300

    categoria

--- src/app/components/private/categoria/categoria.component.ts
+++ src/app/components/private/categoria/categoria.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //import Swal from 'sweetalert2';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormsModule } from '@angular/forms';
 import { CategoriaService } from '../../../services/categoria.service';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-categoria',
@@ -16,15 +16,27 @@ export class CategoriaComponent {
 
   }
 
+  selectedFile: File = null;
+  fd = new FormData();
+
+  createFormData(event) {
+    this.selectedFile = <File>event.target.files[0];
+    this.fd.append('file', this.selectedFile, this.selectedFile.name);
+  }
+
   saveFormCategoria(form: NgForm) {
 
-    this.categoriaService.saveCategoria(form).subscribe(() => {
-      // Swal.fire({
-      //   allowOutsideClick: true,
-      //   icon: 'info',
-      //   text: 'Mensaje enviado correctamente',
-      //   title: 'Formulario de contacto'
-      // });
+    this.fd.append('body', form.value);
+console.log(form.controls['descripcion']);
+
+    this.categoriaService.saveCategoria(this.fd).subscribe(() => {
+
+       Swal.fire({
+         allowOutsideClick: true,
+         icon: 'info',
+         text: 'Mensaje enviado correctamente',
+         title: 'Formulario de contacto'
+       });
     });
 
   }
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -10,6 +10,7 @@ export class CategoriaService {
   constructor(private http: HttpClient) { }
 
   saveCategoria(body){
+
     return this.http.post('http://localhost:3006/insertCategoria', body);
   }
 

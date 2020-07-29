71749c580ae994cb331f789a1428da9f1c089bfb
fboschiero <fboschiero@gmail.com>
2020-07-29 13:39:25 -0300

    baag

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -18,7 +18,7 @@ export class CarritoService {
 
   constructor(private http: HttpClient) { }
   
-carrit  agregarAlCarrito(articulo_id: number, img_articulo: string, color_id: number, talle_id: number, cantidadad: number, precio: number) {
+  agregarAlCarrito(articulo_id: number, img_articulo: string, color_id: number, talle_id: number, cantidadad: number, precio: number) {
     
     const httpOptions = {
       headers: new HttpHeaders({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})

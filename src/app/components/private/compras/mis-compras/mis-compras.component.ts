9e70a736e94601214d85ebe52096972189ba94f8
Pablo <pchaer@gmail.com>
2020-08-17 23:47:08 -0300

    login y registrar

--- src/app/components/private/compras/mis-compras/mis-compras.component.ts
+++ src/app/components/private/compras/mis-compras/mis-compras.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ComprasService } from 'src/app/services/compras.service';
 import { OrdenCompra } from 'src/app/modelsBD/OrdenCompra';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { LoginService } from 'src/app/services/login.service';
+import { Usuario } from 'src/app/modelsBD/Usuario';
 
 @Component({
   selector: 'app-mis-compras',
@@ -10,14 +13,26 @@ import { OrdenCompra } from 'src/app/modelsBD/OrdenCompra';
 export class MisComprasComponent implements OnInit {
 
   listaCompras: OrdenCompra[] = [];
+  p: number = 1;
+  usuario: Usuario;
 
-  constructor(private comprasService: ComprasService) {
+  constructor(private comprasService: ComprasService,
+              private loginService: LoginService) {
 
-    this.listaCompras = this.comprasService.obtenerCompras('p@p', new Date(), new Date());
-
+    let usr = localStorage.getItem('usuario');
+    this.usuario = JSON.parse(usr);
+    
+    // agrego fecha desde y hasta para luego filtrar en un futuro
+    this.listaCompras = this.comprasService.obtenerCompras(this.usuario.email, new Date(), new Date());
+      
   }
 
   ngOnInit(): void {
+  }
+
+  next() {
+    console.log('siguienteeee');
+    
   }
 
 }

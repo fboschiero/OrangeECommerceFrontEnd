1c1819d86d419dfbcb5ca2318e88edf643c02529
fboschiero <fboschiero@gmail.com>
2020-08-21 19:50:35 -0300

    articulos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   API_URL: 'http://localhost:3006',
-  API_URL_IMAGE: 'http://localhost:3006/getImagenProducto/'
+  API_URL_IMAGE: 'http://localhost:3006/getImagenProducto/',
+  PATH_IMAGENES: 'C:\demoorange/'
 };
 
 /*

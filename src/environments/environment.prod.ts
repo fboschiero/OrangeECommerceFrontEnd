82e33c256c2c222f636586f106773e875650423c
fboschiero <fboschiero@gmail.com>
2020-08-29 16:06:40 -0300

    quito path_images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   API_URL: 'http://demo.orangesolutions.uy',
   API_URL_IMAGE: 'http://demo.orangesolutions.uy/getImagenProducto/',
-  PATH_IMAGENES: 'http://demo.orangesolutions.uy/getImagenProducto/'
 };

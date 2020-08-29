1fc342a51b4444e4b7f3838b647f5a5275991bb4
fboschiero <fboschiero@gmail.com>
2020-08-29 15:55:56 -0300

    quito path_imagenes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://demo.orangesolutions.uy',
-  API_URL_IMAGE: 'http://demo.orangesolutions.uy/getImagenProducto/',
-  PATH_IMAGENES: 'http://demo.orangesolutions.uy/getImagenProducto/'
+  API_URL: 'http://localhost:3600',
+  API_URL_IMAGE: 'http://localhost:3600/getImagenProducto/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3006',
-  API_URL_IMAGE: 'http://localhost:3006/getImagenProducto/',
-  PATH_IMAGENES: 'http://localhost:4200/assets/articulos/'
+  API_URL: 'http://localhost:3600',
+  API_URL_IMAGE: 'http://localhost:3600/getImagenProducto/',
 };
 
 /*
